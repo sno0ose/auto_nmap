4f163996fe6bd8899ef4e53657bee7d1be10d63e
joethorpe6 <joethorpe6@openmailbox.org>
2017-04-27 19:21:10 +0100

    moved other scans to their own class and minor bug fixes

--- core/scans.py
+++ core/scans.py
@@ -2,6 +2,78 @@
 
 import os
 
+class portscans():
+    def pingSweep(self):
+        print("#"*92)
+        print("\t\tPING SWEEP of target addresses\t\t")
+        print("\t\tCheck the file 'alive.ip' for all the alive hosts\t\t")
+        print("#"*92)
+    #    os.system('nmap -iL targets.ip -sP -PE -oA scans/PingSweep --excludefile exclude.ip -n --open')
+        os.system('nmap -sn -PE -iL targets.ip -PS3,7,9,13,17,19,21-23,25-26,37,53,79-82,88,100,106,110-111,113,119,135,139,143-144,179,199,254-255,280,311,389,427,443-445,464-465,497,513-515,543-544,548,554,587,593,625,631,636,646,787,808,873,902,990,993,995,1000,1022,1024-1033,1035-1041,1044,1048-1050,1053-1054,1056,1058-1059,1064-1066,1069,1071,1074,1080,1110,1234,1433,1494,1521,1720,1723,1755,1761,1801,1900,1935,1998,2000-2003,2005,2049,2103,2105,2107,2121,2161,2301,2383,2401,2601,2717,2869,2967,3000-3001,3128,3268,3306,3389,3689-3690,3703,3986,4000-4001,4045,4899,5000-5001,5003,5009,5050-5051,5060,5101,5120,5190,5357,5432,5555,5631,5666,5800,5900-5901,6000-6002,6004,6112,6646,6666,7000,7070,7937-7938,8000,8002,8008-8010,8031,8080-8081,8443,8888,9000-9001,9090,9100,9102,9999-10001,10010,32768,32771,49152-49157 -PU53,67-69,111,123,135,137-139,161-162,445,500,514,520,631,996-999,1434,1701,1900,3283,4500,5353,49152-49154 -oA scans/PingSweep --excludefile exclude.ip --min-hostgroup 256 --min-rate=2000 --open')
+    #    os.system('cat scans/PingSweep.gnmap | awk \'/Up/{print $2}\' >> alive.ip')
+        os.system('grep "Up" scans/PingSweep.gnmap | cut -d " " -f2 |sort -u > alive.ip')
+    
+    def portScan(self):
+        print("#"*92)
+        print("\t\tPORT SCAN of target addresses\t\t")
+        print("#"*92)
+        os.system('nmap -iL alive.ip -sTU -T4 -A -Pn -n -oA scans/portscan -v -p T:3,7,9,13,17,19,21-23,25-26,37,53,79-82,88,100,106,110-111,113,119,135,139,143-144,179,199,254-255,280,311,389,427,443-445,464-465,497,513-515,543-544,548,554,587,593,625,631,636,646,787,808,873,902,990,993,995,1000,1022,1024-1033,1035-1041,1044,1048-1050,1053-1054,1056,1058-1059,1064-1066,1069,1071,1074,1080,1110,1234,1433,1494,1521,1720,1723,1755,1761,1801,1900,1935,1998,2000-2003,2005,2049,2103,2105,2107,2121,2161,2301,2383,2401,2601,2717,2869,2967,3000-3001,3128,3268,3306,3389,3689-3690,3703,3986,4000-4001,4045,4899,5000,5003,5009,5050-5051,5060,5101,5120,5190,5357,5432,5555,5631,5666,5800,5900-5901,6000-6002,6004,6112,6646,6666,7000,7070,7937-7938,8000,8002,8008-8010,8031,8080-8081,8443,8888,9000-9001,9090,9100,9102,9999-10001,10010,32768,32771,49152-49157,U:53,69,123,161,500,1434 --min-hostgroup 256 --min-rate=2000')
+    
+    def portScanAllPorts(self):
+        print("#"*92)
+        print("\t\tPORT SCAN of target addresses with all TCP and UDP ports\t\t")
+        print("#"*92)
+        os.system('nmap -iL alive.ip -sTU -T4 -A -Pn -n -oA scans/portscanAll -v -p T:0-65535,U:0-65535 --min-hostgroup 256 --min-rate=2000')
+    
+    def portScanAllTcpPorts(self):
+        print("#"*92)
+        print("\t\tPORT SCAN of target addresses with all TCP ports\t\t")
+        print("#"*92)
+        os.system('nmap -iL alive.ip -sTU -T4 -A -Pn -n -oA scans/portscanAllTcp -v -p T:0-65535,U:53,69,123,161,500,1434 --min-hostgroup 256 --min-rate=2000')
+
+class parser():
+    # simply parses the pingsweep.gnmap file and places any open ports into a text file with the respective IP address in it.
+    def nmapScrape(self):
+        print("#"*92)
+        print("\tCheck files 'IP.txt' in the open-ports folder\t")
+        print("#"*92)
+        os.system('./nmapscrape.py scans/portscan.gnmap')
+    
+        # simply parses the pingsweep.gnmap file and places any open ports into a text file with the respective IP address in it.
+    def nmapScrapeAllPorts(self):
+        print("#"*92)
+        print("\tCheck files 'IP.txt' in the open-ports folder\t")
+        print("#"*92)
+        os.system('./nmapscrape.py scans/portscanAll.gnmap')
+    
+        # simply parses the pingsweep.gnmap file and places any open ports into a text file with the respective IP address in it.
+    def nmapScrapeAllTcpPorts(self):
+        print("#"*92)
+        print("\tCheck files 'IP.txt' in the open-ports folder\t")
+        print("#"*92)
+        os.system('./nmapscrape.py scans/portscanAllTcp.gnmap')
+    
+    # calls nmap_parser.py and writes output to the directory enumeration with filename nmapreport.txt
+    def nmapParser(self):
+        print("#"*92)
+        print("\t\tCheck the file 'nmapreport' in the enumeration folder\t\t")
+        print("#"*92)
+        os.system('./nmap_parser.py scans/portscan.gnmap > enumeration/nmapreport.txt')
+    
+    # calls nmap_parser.py and writes output to the directory enumeration with filename nmapreport.txt
+    def nmapParserAllPorts(self):
+        print("#"*92)
+        print("\t\tCheck the file 'nmapreport' in the enumeration folder\t\t")
+        print("#"*92)
+        os.system('./nmap_parser.py scans/portscanAll.gnmap > enumeration/nmapreport.txt')
+    
+    # calls nmap_parser.py and writes output to the directory enumeration with filename nmapreport.txt
+    def nmapParserAllTcpPorts(self):
+        print("#"*92)
+        print("\t\tCheck the file 'nmapreport' in the enumeration folder\t\t")
+        print("#"*92)
+        os.system('./nmap_parser.py scans/portscanAllTcp.gnmap > enumeration/nmapreport.txt')
+
 class nsescans():
     def snmpEnum(self):
         if os.path.exists('open-ports/161.txt'):
@@ -192,7 +264,7 @@ class nsescans():
             ORACLE='nmap --script=oracle-sid-brute -p 1521-1560 -iL open-ports/1521.txt -oN nse_scans/oracle --stats-every 60s --min-hostgroup 256 --min-rate=2000'
             os.system(ORACLE)
     
-    #def slowlorisEnum():
+    #def slowlorisEnum(self):
     #    if os.path.exists('open-ports/80.txt'):
     #        print("#"*61)
     #        print("            Running NSE script for slowloris                ")
@@ -200,3 +272,53 @@ class nsescans():
     #        print("#"*61)
     #        SLOWLORIS='nmap --script http-slowloris-check -iL open-ports/80.txt -oN nse_scans/slowloris --stats-every 60s --min-hostgroup 256 --min-rate=2000'
     #        os.system(SLOWLORIS)
+
+class misc():
+# for tests that either are not nmap or use nmap + other tools
+    def ikeEnum(self):
+        if os.path.exists('open-ports/500.txt'):
+            print("#"*61)
+            print("\tRunning NSE script for IKE\t")
+            print("\tCheck the the IKE file in the nse_scans directory\t")
+            print("#"*61)
+            IKE='nmap -sU -p 500 -iL open-ports/500.txt --script=ike-version -oN nse_scans/ike --stats-every 60s --min-hostgroup 256 --min-rate=2000'
+            os.system(IKE)
+            os.makedirs('nse_scans/IKE') # Make IKE directory for aggressive mode output
+            ### Below: Run aggressive IKE scan and print to directory with the IP address found as filename ###
+            with open("open-ports/500.txt","r") as f:
+                ip_address = f.readline().rstrip()
+                while ip_address != "":
+                    os.system("ike-scan -A -M %s --id=GroupVPN > nse_scans/IKE/%s.txt" % (ip_address,ip_address))
+                    #print ('ip_address')
+                    ip_address = f.readline().rstrip()
+            f.close()
+    
+    def ms15034Enum(self):
+        if os.path.exists('open-ports/443.txt'):
+            print("#"*61)
+            print("\tRunning MS15_034 file test\t")
+            print("#"*61)
+            os.makedirs('nse_scans/MS15034') # Make directory for output
+            ### Below: Run ms15_034 check scan ###
+            with open("open-ports/443.txt","r") as f:
+                ip_address = f.readline().rstrip()
+                while ip_address != "":
+                    os.system('curl -v https://%s/ -H "Host: hostname" -H "Range: bytes=0-18446744073709551615" -k > nse_scans/MS15034/%s' % (ip_address,ip_address))
+                    #print ('ip_address')
+                    ip_address = f.readline().rstrip()
+            f.close()
+    
+    # Check for MS14_066 over 443 but only IP's in 3389 - since we can't validate the ciphers over 3389
+    def ms14066Enum(self):
+        if os.path.exists('open-ports/3389.txt'):
+            print("#"*61)
+            print("\tRunning WinShock.sh file test for MS14_066\t")
+            print("#"*61)
+            os.makedirs('nse_scans/MS14066') # Make IKE directory for aggressive mode output
+            ### Below: Run winschock scan ###
+            with open("open-ports/3389.txt","r") as f:
+                winshock = f.readline().rstrip()
+                while winshock != "":
+                    os.system("./winshock.sh %s > nse_scans/MS14066/%s.txt" % (winshock,winshock))
+                    winshock = f.readline().rstrip()
+            f.close()
